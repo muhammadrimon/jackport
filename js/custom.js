b358cf974997969f39cbda4dd6fbeeca23c7005d
muhammadrimon <course.creativeit@gmail.com>
2022-03-25 07:21:02 -0600

    or change

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,28 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  "use strict";
+
+  //   $(".demo").beefup({
+  //     accessibility: true,
+  //   });
+
+  //   up down create;
+  $(".item1 .faq-heading").on("click", function () {
+    $(".item1 .faq-content").slideToggle();
+    $(".item1").toggleClass("border-red");
+  });
+
+  $(".item2 .faq-heading").on("click", function () {
+    $(".item2 .faq-content").slideToggle();
+    $(".item2").toggleClass("border-red");
+  });
+
+  $(".item3 .faq-heading").on("click", function () {
+    $(".item3 .faq-content").slideToggle();
+    $(".item3").toggleClass("border-red");
+  });
+
+  $(".item4 .faq-heading").on("click", function () {
+    $(".item4 .faq-content").slideToggle();
+    $(".item4").toggleClass("border-red");
+  });
+});
