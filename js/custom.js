1701036afceabb451d1fc7e7ed7602c2a479be30
muhammadrimon <course.creativeit@gmail.com>
2022-03-27 05:48:08 -0600

    or change

--- js/custom.js
+++ js/custom.js
@@ -9,20 +9,28 @@ $(document).ready(function () {
   $(".item1 .faq-heading").on("click", function () {
     $(".item1 .faq-content").slideToggle();
     $(".item1").toggleClass("border-red");
+    $(".content2, .content3, .content4").hide();
+    $(".item2, .item3, .item4").removeClass("border-red", 1000);
   });
 
   $(".item2 .faq-heading").on("click", function () {
     $(".item2 .faq-content").slideToggle();
     $(".item2").toggleClass("border-red");
+    $(".content1, .content3, .content4").hide();
+    $(".item1, .item3, .item4").removeClass("border-red", 1000);
   });
 
   $(".item3 .faq-heading").on("click", function () {
     $(".item3 .faq-content").slideToggle();
     $(".item3").toggleClass("border-red");
+    $(".content2, .content1, .content4").hide();
+    $(".item2, .item1, .item4").removeClass("border-red", 1000);
   });
 
   $(".item4 .faq-heading").on("click", function () {
     $(".item4 .faq-content").slideToggle();
     $(".item4").toggleClass("border-red");
+    $(".content2, .content3, .content1").hide();
+    $(".item2, .item3, .item1").removeClass("border-red", 1000);
   });
 });
